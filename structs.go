8fbce4cf07e182560ae6ed430ab1a1f99dd4f150
ylee <leeyn.shun@gmail.com>
2025-01-15 18:59:32 +0200

    Create some structs

---
+++ structs.go
@@ -0,0 +1,37 @@
+package main
+
+import "time"
+
+type user struct {
+	typeID    int
+	name      string
+	email     string
+	pwHash    string
+	regDate   time.Time
+	lastLogin time.Time
+}
+
+type session struct {
+	id         string
+	userID     int
+	isActive   bool
+	startTime  time.Time
+	expireTime time.Time
+	lastAccess time.Time
+}
+
+type post struct {
+	ID           int
+	UserID       int
+	UserName     string
+	CommentCount int
+	LikeCount    int
+	DislikeCount int
+	Title        string
+	Content      string
+	CreatedAt    time.Time
+}
+
+type posts struct {
+	index []post
+}
