d043e6998f6cdcfc9d683453d31653e15a98d4f6
JSundB <johannes.sundback.2002@gmail.com>
2025-01-16 12:54:08 +0200

    did some simple login and register data handling

--- utils/pages.go
+++ utils/pages.go
@@ -35,7 +35,22 @@ func LoginPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		CustomExecuteTemplate(w, "login.html", nil)
 	} else if r.Method == http.MethodPost {
-		//	Trying to log in, check if credentials are valid and check if they already exist in database
+		if err := r.ParseForm(); err != nil {
+			//	Error parsing data
+		}
+		loginData := LoginData{
+			username: r.FormValue("name"),
+			password: r.FormValue("password"),
+		}
+		nameIsValid, _ := CheckValidity(loginData.username, "username")
+		passIsValid, _ := CheckValidity(loginData.password, "password")
+		if nameIsValid && passIsValid {
+			fmt.Println("Valid user, check if duplicate")
+		} else {
+			//	Placeholder:
+			fmt.Println("Invalid name, or pass.")
+			http.Error(w, "Invalid name, or pass.", http.StatusBadRequest)
+		}
 	} else {
 		//	Error wrong method
 	}
@@ -46,7 +61,29 @@ func RegisterPage(w http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		CustomExecuteTemplate(w, "register.html", nil)
 	} else if r.Method == http.MethodPost {
-		//	Trying to register, check if credentials are valid and check if they already exist in database
+		if err := r.ParseForm(); err != nil {
+			//	Error parsing data
+		}
+		if r.FormValue("password") != r.FormValue("passwordConf") {
+			//	Placeholder:
+			fmt.Println("The two passwords given are different")
+			http.Error(w, "The two password are different.", http.StatusBadRequest)
+		}
+		registerData := RegisterData{
+			username: r.FormValue("name"),
+			password: r.FormValue("password"),
+			email:    r.FormValue("email"),
+		}
+		nameIsValid, _ := CheckValidity(registerData.username, "username")
+		passIsValid, _ := CheckValidity(registerData.password, "password")
+		emailIsValid, _ := CheckValidity(registerData.email, "email")
+		if nameIsValid && passIsValid && emailIsValid {
+			fmt.Println("Valid user, check if duplicate")
+		} else {
+			//	Placeholder:
+			fmt.Println("Invalid name, pass, or email.")
+			http.Error(w, "Invalid name, pass, or email.", http.StatusBadRequest)
+		}
 	} else {
 		//	Error wrong method
 	}
