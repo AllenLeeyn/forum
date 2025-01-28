76737bbef482152ebd52ba00a911c522f24c7258
atouba <anasstouba5@gmail.com>
2025-01-28 16:39:53 +0200

    fix error

--- handlers/feedback.go
+++ handlers/feedback.go
@@ -3,8 +3,7 @@ package handlers
 import "net/http"
 
 func Feedback(w http.ResponseWriter, r *http.Request) {
-	sessionCookie, _ := r.Cookie("session-id")
-	userID := checkSessionValidity(w, sessionCookie)
+	_, userID := checkSessionValidity(w, r)
 	if userID == -1 { // likely user not login
 		w.WriteHeader(http.StatusNotFound)
 		return
