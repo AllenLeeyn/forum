4d930795158d6cdf295b0b91f9a734de14c18ec8
Eliza John <112093038+ejmilli@users.noreply.github.com>
2025-01-30 23:53:21 +0200

    created an about page to add our data into it, also added images and links to it

---
+++ handlers/about.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"net/http"
+	"text/template"
+)
+
+
+
+ func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	
+
+	// Parse template
+	tmpl, err := template.ParseFiles("templates/about.html")
+	if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+	}
+
+	// Execute template
+	err = tmpl.Execute(w, "about.html")
+	if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
